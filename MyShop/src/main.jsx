e338bba93bcdebcc622f352344c090b1ee58f666
Junseok Ahn <rrvvy9595@gmail.com>
2025-07-17 01:28:21 +0900

    Feat: Vercel 분석 기능 추가 및 배포 에러 수정

--- MyShop/src/main.jsx
+++ MyShop/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,8 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </QueryClientProvider>
   </StrictMode>,
 );
